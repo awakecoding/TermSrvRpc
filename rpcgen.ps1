ddc4abbc20aca4fa38d96893d32afc05f8bcd1b8
Marc-André Moreau <mamoreau@devolutions.net>
2025-07-01 17:00:21 -0400

    add more idl files

--- rpcgen.ps1
+++ rpcgen.ps1
@@ -1,17 +1,16 @@
 
-$PredefExtra = '
-#include <stdlib.h>
-static void* MIDL_user_allocate(size_t sz) { return malloc(sz); }
-static void MIDL_user_free(void* ptr) { free(ptr); }'
-
 foreach ($Arch in @('x64', 'arm64')) {
     New-Item -Type Directory -Path "idl/$Arch" -ErrorAction SilentlyContinue
     Copy-Item ./idl/ms-dtyp.h ./idl/$Arch/ms-dtyp.h -Force
 
-    foreach ($ModuleName in @('SessEnvRpc')) {
+    foreach ($ModuleName in @('SessEnvRpc', 'rcmpublic', 'tspubrpc', 'TSVIPRpc')) {
         midl.exe "./idl/$ModuleName.idl" /I idl /notlb /header "$ModuleName.h" /env $Arch /server none /out "./idl/$Arch"
-        $ClientStub = Get-Content -Path "./idl/$Arch/${ModuleName}_c.c" -Raw
-        $ClientStub = $PredefExtra + $ClientStub
-        Set-Content -Path "./idl/$Arch/${ModuleName}_c.c" -Value $ClientStub
+        
+        $stubFile = "idl/${Arch}/${ModuleName}_c.c"
+        if (Test-Path $stubFile) {
+            (Get-Content $stubFile) | ForEach-Object {
+                $_ -replace '^(VOID\s+Opnum\d+NotUsedOnWire)', 'static $1'
+            } | Set-Content $stubFile
+        }
     }
 }
